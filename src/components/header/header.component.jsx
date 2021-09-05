5041362ad079679bdf5e6b18b78650f1f3a6a53b
bridget462 <bridges.dev.create@gmail.com>
2021-09-05 16:59:36 +0900

    updating our app to redirect to home if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,7 +18,12 @@ const Header = ({ currentUser }) => (
         CONTACT
       </Link>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+        <div
+          className="option"
+          onClick={() => {
+            auth.signOut();
+          }}
+        >
           SIGN OUT
         </div>
       ) : (
