f607b3a3c99e106fbc23ef59d5247e3bcb2b2ac6
bridget462 <bridges.dev.create@gmail.com>
2021-09-19 18:17:09 +0900

    loading wrapper component added for shop item fetching time

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,8 +8,17 @@ import {
 } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 import { updateCollections } from "../../redux/shop/shop.actions";
+import withSpinner from "../../components/with-spinner/with-spinner.component";
+
+const CollectionsOverviewWithSpinner = withSpinner(CollectionOverview);
+const CollectionPageWithSpinner = withSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+  // react automatically define state at constructor
+  state = {
+    loading: true,
+  };
+
   // we have to both subscribe to snapshot at mount, as well as unsubscribe at unmount
   unsubscribeFromSnapShot = null;
 
@@ -23,18 +32,28 @@ class ShopPage extends React.Component {
         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
         // console.log(collectionsMap);
         updateCollections(collectionsMap);
+        this.setState({ loading: false });
       }
     );
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="show-page">
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
